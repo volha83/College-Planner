02e038b9dd66066b87f1caac3bf4af730151a12b
Volha Padlipskaya <o.podlipskaya@Mac.lan>
2025-10-12 23:19:59 -0400

    created project with Vite + React

---
+++ README.md
@@ -0,0 +1,5 @@
+Final Project for React course with Code the Dream
+
+#
+
+College Planner - Your Admission & College Planner
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1> College Planner</h1>
+      <p> text </p>
+    </div>
+  );
+}
+
+export default App;
