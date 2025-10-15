c0288c931ea764b61b9626a935e7e549c6fd0078
Volha Padlipskaya <o.podlipskaya@Mac.lan>
2025-10-15 16:28:21 -0400

    beginning

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css';
 import React from 'react';
+// import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+  console.log('cod', import.meta.env.VITE_AIRTABLE_BASE_ID);
+  console.log('name table', import.meta.env.VITE_TABLE_NAME);
+  console.log('all:', import.meta.env);
   return (
     <div>
       <h1> College Planner</h1>
